0689da8f3b02db822f477f007a80f1cd4e87662f
Erfan Ebrahimnia <erfan.ebrahimnia@massiveart.com>
2014-06-19 20:29:01 +0200

    initial commit

---
+++ scripts/background.js
@@ -0,0 +1,145 @@
+(function($, window, document) {
+    'use strict';
+
+    var Notification = webkitNotifications;
+    var Twitch = Twitch || {};
+    var Storage = chrome.storage.local;
+
+    const CONFIG = {
+        updateInterval: 5000
+    };
+
+    const API = Twitch.API = {
+        baseUrl: 'https://api.twitch.tv/kraken/streams/',
+        mock: '../api-mocks/'
+    };
+
+    function showNotification(avatar, title, text) {
+        var notification = Notification.createNotification(avatar, title, text);
+        notification.show();
+
+        setTimeout(function() {
+            notification.close
+        }, 5000);
+    }
+
+    Twitch.Channel = function() {
+        this.isOnline = false;
+    };
+
+    Twitch.Channel.prototype.fetch = function fetch(channel) {
+        var deferred = new $.Deferred();
+        
+        $.getJSON(Twitch.API.baseUrl + channel, function(response) {
+            var oldChannelData = $.extend({}, this);
+
+            this.parse(response);
+            deferred.resolve(this, oldChannelData);
+        }.bind(this));
+
+        return deferred.promise();
+    };
+
+    Twitch.Channel.prototype.parse = function parse(response) {
+        var stream = response.stream;
+        this.isOnline = !!stream;
+
+        if (this.isOnline) {
+            this.name = stream.channel.display_name;
+            this.logo = stream.channel.logo;
+            this.game = stream.channel.game;
+            this.url = stream.channel.url;
+            this.status = stream.channel.status;
+            this.viewers = stream.viewers;
+            this.preview = stream.preview;
+        } 
+    };
+
+    Twitch.Extension = function() {
+        var channels = [];
+
+        function isSubscribed(channelName) {
+            return channels.indexOf(channelName) !== -1;
+        }
+
+        return {
+            start: function() {
+                Storage.get({ channels: [] }, function(result) {
+                    channels = result.channels;
+                    this.startUpdateLoop();
+                    this.listenForMessages();
+                }.bind(this));
+            },
+
+            startUpdateLoop: function() {
+                this.update();
+                setInterval(this.update.bind(this), CONFIG.updateInterval);
+            },
+
+            channels: {},
+
+            update: function() {
+                var channel;
+
+                channels.forEach(function(channelName) {
+                    channel = this.channels[channelName] || new Twitch.Channel;
+
+                    channel.fetch(channelName).done(function(updatedChannelData, oldChannelData) {
+                        if (!this.channels[channelName]) {
+                            this.channels[channelName] = channel;
+                        } else {
+                            this.checkForUpdates(updatedChannelData, oldChannelData);
+                        }
+                    }.bind(this));
+                }.bind(this));
+            },
+
+            checkForUpdates: function(updatedChannelData, oldChannelData) {
+                if (updatedChannelData.isOnline && updatedChannelData.isOnline !== oldChannelData.isOnline) {
+                    showNotification(
+                        updatedChannelData.logo, 
+                        updatedChannelData.name + ' is streaming',
+                        updatedChannelData.status
+                    );
+                }
+            },
+
+            listenForMessages: function() {
+                chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+                    switch (request.type) {
+                        case 'initial':
+                            this.handleInitialMessage(request); 
+                            break;
+                        case 'toggleSubscription':
+                            this.handleSubscription(request);
+                            break;
+                    }
+                }.bind(this));
+            },
+
+            handleInitialMessage: function(request) {
+                var subscribed = isSubscribed(request.channelName);
+
+                chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, { channelName: request.channelName, type: 'initial', subscribed: subscribed });
+                });
+            },
+
+            handleSubscription: function(request) {
+                var channelIndex;
+
+                if (isSubscribed(request.channelName)) {
+                    channelIndex = channels.indexOf(request.channelName);
+                    channels.splice(channelIndex, 1);
+                } else {
+                    channels.push(request.channelName);
+                }
+
+                Storage.set({ channels: channels });
+            }
+        };
+    };
+
+    Twitch.Extension().start();
+
+})(jQuery, window, document)
---
+++ scripts/content-script.js
@@ -0,0 +1,56 @@
+(function(window) {
+    var initInterval = setInterval(function() {
+        if (!!document.querySelector('.channel-actions')) {
+            init();
+            clearInterval(initInterval);
+        }
+    }, 500);
+
+    function getChannelName() {
+        var pathName = window.location.pathname;
+        return pathName.substr(1);
+    }
+
+    function displayTurnOnButton(button) {
+        button.innerHTML = 'Show Browser Notifications';
+    }
+
+    function displayTurnOffButton(button) {
+        button.innerHTML = 'Stop Browser Notifications';
+    }
+
+    function init() {
+        var channelName = getChannelName();
+        var container = document.querySelector('.channel-actions');
+        var button = document.createElement('button');
+        var subscribed = false;
+
+        button.className = 'button primary';
+
+        displayTurnOnButton(button);
+
+        chrome.runtime.sendMessage({ channelName: channelName, type: 'initial' });
+
+        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+            if (request.type === 'initial') {
+                if (request.subscribed) {
+                    displayTurnOffButton(button);
+                    subscribed = true;
+                }
+            }
+            container.appendChild(button);
+        });
+
+        button.addEventListener('click', function() {
+            chrome.runtime.sendMessage({ channelName: channelName, type: 'toggleSubscription' });
+
+            if (subscribed) {
+                subscribed = false;
+                displayTurnOnButton(button);
+            } else {
+                subscribed = true;
+                displayTurnOffButton(button);
+            }
+        }, false);
+    }
+})(window)
